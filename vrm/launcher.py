a88524a1364ae704048ce010b032bfa6e34be467
muchu1983 <muchu1983@gmail.com>
2025-01-11 11:53:57 +0800

    add re module

--- vrm/launcher.py
+++ vrm/launcher.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
 
 if __name__ == "__main__":
     with open('./res/inputs/D0035_01.txt', 'r') as file:
         for line in file:
             print(line)
+            if re.search('設定', line):
+                print(re.search('設定', line).group())
+            else:
+                print(None)
