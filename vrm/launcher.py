a23f683682b7e614863cb8fa723c71ee6268174a
muchu1983 <muchu1983@gmail.com>
2025-01-11 11:37:27 +0800

    add res folder

---
+++ vrm/launcher.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    with open('./res/inputs/D0035_01.txt', 'r') as file:
+        for line in file:
+            print(line)
