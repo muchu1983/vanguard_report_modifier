c4c2eb8a622daa44641ce0d0f26a220b84d4d10b
muchu1983 <muchu1983@gmail.com>
2025-01-19 20:03:10 +0800

    add area

--- vrm/launcher.py
+++ vrm/launcher.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import re
 from vrm.line_wrapper import LineWrapper
 
 if __name__ == "__main__":
     with open('./res/inputs/D0233.txt', 'r') as file:
         for line in file:
             lw = LineWrapper(line)
-            lw.is_word_in_line('(設定)|(解除)|(巡查)')
+            lw.is_word_in_line('(設定)|(解除)|(巡查)|(第 1 區)|(第 2 區)')
