37e1e72c79b68eaed77109d2cf56dc6b1df2d979
muchu1983 <muchu1983@gmail.com>
2025-02-01 09:29:58 +0800

    add txt_wrapper.py

---
+++ vrm/txt_wrapper.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+from vrm.line_wrapper import LineWrapper
+
+class TxtWrapper(object):
+
+    def __init__(self, file_path):
+        super(TxtWrapper, self).__init__()
+        self.txt_path = file_path
+        self.lines = []
+        self.remove_bom(self.txt_path)
+
+    def remove_bom(self, file_path):
+        content = None
+        with open(file_path, 'r', encoding='utf-8-sig') as file:
+            content = file.read()
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(content)
+
+    def load_lines(self):
+        with open(self.txt_path, 'r') as file:
+            for line in file:
+                lw = LineWrapper(line)
+                self.lines.append(lw)
+            print(len(self.lines))
+
+    def list_duplicate_line(self):
+        pre_line_word = None
+        for lw in self.lines:
+            line_word = lw.is_word_in_line('(設    定)|(設定)|(解    除)|(解除)|(巡   查)|(巡查)')
+            if line_word != pre_line_word:
+                pass
+            elif line_word is not None and line_word == pre_line_word:
+                lw.print_line()
+            pre_line_word = line_word
